64b6adbaf16acfa925c6c64d5553f6cf4d75b5a7
Leandro <leandro.soares.eneterio@gmail.com>
2025-05-11 08:49:02 -0300

    0.3.5 ratelimit ok

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Page = () => {
+    return (
+        <main className="flex-1 bg-pattern bg-cover bg-top bg-dark-100 px-5 xs:px-10 md:px-16 flex min-h-screen flex-col items-center justify-center">
+            <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+                Eita! Tá com pressa, hein? 🏃‍♂️💨
+            </h1>
+            <p className="mt-3 max-w-xl text-center text-light-400">
+                Parece que você se empolgou demais 😅. Demos uma pausa temporária na sua animação. 🚦 Respira um pouco e tenta de novo em instantes
+            </p>
+        </main>
+    );
+};
+export default Page;
